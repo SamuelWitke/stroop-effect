a014d6e706c57477f033bb1cd0e9174f57754981
SamuelWitke <SamuelWitke@gmail.com>
2018-04-10 22:53:53 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Index from './scenes/HomePage/index.js'
+import {colors} from './colors.js'
+import Paper from 'material-ui/Paper';
+import {List, ListItem} from 'material-ui/List';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props){
+        super(props);
+        this.onButtonClick = this.onButtonClick.bind(this)
+    }
+    onButtonClick(color,choice){
+        console.log(color,choice)
+        alert(color == choice)
+    }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <Paper className="container">
+                    <List>
+                    {
+                        colors.map( (color,i) => (
+                            <ListItem>
+                            <div key={i}>
+                            <Index 
+                                color={color} 
+                                i={i}
+                                onClick={this.onButtonClick}
+                            />
+                        </div>
+                    </ListItem>
+                        ))
+                    }
+                </List>
+                </Paper>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
